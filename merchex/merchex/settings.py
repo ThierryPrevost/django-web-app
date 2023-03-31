790abe6f92273972709dedd7854d095895360a71
thierry prevost <thierryprevost333@gmail.com>
2023-03-31 20:53:12 +0200

    commit 01

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x*rpz2aofjre1xi^aj33slg59o-w0=px0wk#%=s=8nba5eam$#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
